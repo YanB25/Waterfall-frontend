9bd5ebdbbeecf0c71432f97cdf6cc7bc8b7c15c5
yanbin <yanb25@mail2.sysu.edu.cn>
2019-05-06 14:26:30 +0800

    change login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,99 @@
 <template>
-    <div> hello world </div>
+  <el-form
+    :model="ruleForm"
+    status-icon
+    :rules="rules"
+    ref="ruleForm"
+    label-width="120px"
+    class="demo-ruleForm"
+  >
+    <el-form-item label="User Name" prop="username">
+      <el-input v-model.number="ruleForm.username"></el-input>
+    </el-form-item>
+    <el-form-item label="Password" prop="pass">
+      <el-input type="password" v-model="ruleForm.pass" auto-complete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="Confirm" prop="checkPass">
+      <el-input type="password" v-model="ruleForm.checkPass" auto-complete="off"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('ruleForm')">Submit</el-button>
+      <el-button @click="resetForm('ruleForm')">Reset</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
+
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+interface Callback {
+    (err?: Error): any
+};
+
+interface LoginComponent extends Vue{
+  resetFields(): void,
+  validate(valid: object): boolean
+
+}
 
 @Component
 export default class Login extends Vue {
-}
+    checkUsername(rule: object, value :number, callback: Callback) {
+      if (!value) {
+        return callback(new Error("Please input user name"));
+      }
+      setTimeout(() => {
+        callback();
+      }, 1000);
+    }
+    validatePass(rule: object, value: string, callback: Callback) {
+      if (value === "") {
+        callback(new Error("Please input the password"));
+      } 
+      else if (value.length < 6) {
+        callback(new Error("Place enter password longer than 6 characters"))
+      }
+      else {
+        if (this.ruleForm.checkPass !== "") {
+          (this.$refs.ruleForm as any).validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    validatePass2(rule: object, value: string, callback: Callback) {
+      if (value === "") {
+        callback(new Error("Please input the password again"));
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error("Two inputs don't match!"));
+      } else {
+        callback();
+      }
+    };
+    ruleForm = {
+      pass: "",
+      checkPass: "",
+      username: "",
+    }
+    rules = {
+      pass: [{ validator: this.validatePass, trigger: "blur" }],
+      checkPass: [{ validator: this.validatePass2, trigger: "blur" }],
+      username: [{ validator: this.checkUsername, trigger: "blur" }]
+    }
+    submitForm(formName: string) {
+      console.log(this.$refs[formName]);
+      (this.$refs[formName] as LoginComponent).validate((valid: any) => {
+        if (valid) {
+          alert("submit!");
+          return true
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+    resetForm(formName: string) {
+      (this.$refs[formName] as LoginComponent).resetFields();
+    }
+};
 </script>
