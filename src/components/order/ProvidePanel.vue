a254878d0c61273dec77eb633ab287df7c4006da
gtbl2012 <admin@gtbl2012.cn>
2019-06-01 20:01:32 +0800

    add confirm

--- src/components/order/ProvidePanel.vue
+++ src/components/order/ProvidePanel.vue
@@ -12,7 +12,11 @@
       <el-input v-model="ruleForm.mainorder" :disabled="true"></el-input>
     </el-form-item>
     <el-form-item label="Quantity" prop="quantity">
-      <el-input v-model="ruleForm.quantity" auto-complete="off" @keyup.enter="submitForm('fuleForm')"></el-input>
+      <el-input
+        v-model="ruleForm.quantity"
+        auto-complete="off"
+        @keyup.enter="submitForm('fuleForm')"
+      ></el-input>
     </el-form-item>
     <el-form-item label="Comments" prop="comments">
       <el-input v-model="ruleForm.comments" type="textarea" :rows="3" placeholder="Optinal"></el-input>
@@ -21,7 +25,7 @@
       <el-input v-model="ruleForm.phone" placeholder="Optional"></el-input>
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="submitForm('ruleForm')" >Submit</el-button>
+      <el-button type="primary" @click="submitForm('ruleForm')">Submit</el-button>
       <el-button @click="resetForm('ruleForm')">Reset</el-button>
     </el-form-item>
   </el-form>
@@ -30,87 +34,103 @@
 
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
-import validator from 'validator';
+import validator from "validator";
 
 interface Callback {
-    (err?: Error): any
-};
+  (err?: Error): any;
+}
 
-interface LoginComponent extends Vue{
-  resetFields(): void,
-  validate(valid: object): boolean
+interface LoginComponent extends Vue {
+  resetFields(): void;
+  validate(valid: object): boolean;
 }
 
 @Component
 export default class Login extends Vue {
-    @Prop() quantity!: number;
-    userid: string = sessionStorage.getItem('userid') as string;
-    ruleForm = {
-      mainorder: '',
-      comments: '',
-      quantity: '',
-      phone: '',
+  @Prop() quantity!: number;
+  userid: string = sessionStorage.getItem("userid") as string;
+  ruleForm = {
+    mainorder: "",
+    comments: "",
+    quantity: "",
+    phone: ""
+  };
+  beforeMount() {
+    this.ruleForm.mainorder = this.$route.params.orderid;
+  }
+  checkNothing(a: object, b: object, callback: Callback) {
+    callback();
+  }
+  checkNumber(rule: Object, value: string, callback: Callback) {
+    if (!validator.isNumeric(value)) {
+      callback(new Error("This field should be digits"));
+      return;
     }
-    beforeMount() {
-        this.ruleForm.mainorder = this.$route.params.orderid;
+    callback();
+  }
+  checkPhone(rule: Object, value: string, callback: Callback) {
+    if (value == "") {
+      callback();
+      return;
     }
-    checkNothing(a: object, b: object, callback: Callback) { callback(); }
-    checkNumber(rule: Object, value: string, callback: Callback) {
-        if (!validator.isNumeric(value)) {
-            callback(new Error('This field should be digits'));
-            return;
-        }
-        callback();
+    if (!validator.isMobilePhone(value, "any")) {
+      callback(new Error("Please enter a valid phone number"));
+      return;
     }
-    checkPhone(rule: Object, value: string, callback: Callback) {
-        if (value == '') {
-            callback();
-            return;
-        }
-        if (!validator.isMobilePhone(value, 'any')) {
-            callback(new Error('Please enter a valid phone number'));
-            return;
-        }
-        callback();
-    }
-    rules = {
-        comments: [{ validator: this.checkNothing, trigger: "blur"}],
-        quantity: [{ validator: this.checkNumber, trigger: "blur"}],
-        phone: [{ validator: this.checkPhone, trigger: "blur"}]
-    }
-    submitForm(formName: string) {
-      console.log(formName);
-      (this.$refs[formName] as LoginComponent).validate((valid: any) => {
-        if (valid) {
-            console.log({...this.ruleForm, ...{userid: this.userid}});
+    callback();
+  }
+  rules = {
+    comments: [{ validator: this.checkNothing, trigger: "blur" }],
+    quantity: [{ validator: this.checkNumber, trigger: "blur" }],
+    phone: [{ validator: this.checkPhone, trigger: "blur" }]
+  };
+  submitForm(formName: string) {
+    console.log(formName);
+    (this.$refs[formName] as LoginComponent).validate((valid: any) => {
+      if (valid) {
+        this.$confirm("你确定要提交吗?", "提交确认", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        })
+          .then(() => {
+            console.log({ ...this.ruleForm, ...{ userid: this.userid } });
             fetch(`/api/order/subOrder`, {
-                method: 'POST',
-                body: JSON.stringify({
-                    ...this.ruleForm,
-                    ...{ createuser: this.userid }
-                })
-            }).then(res => res.json())
-            .then(res => {
-                if (res.code === 0) {
-                    this.$message({
-                        message: "success",
-                        type: "error"
-                    })
-                } else {
-                    this.$message({
-                        message: `err: ${res.msg}`,
-                        type: "error"
-                    })
-                }
+              method: "POST",
+              body: JSON.stringify({
+                ...this.ruleForm,
+                ...{ createuser: this.userid }
+              })
             })
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
-    }
-    resetForm(formName: string) {
-      (this.$refs[formName] as LoginComponent).resetFields();
-    }
-};
+              .then(res => res.json())
+              .then(res => {
+                if (res.code === 0) {
+                  this.$message({
+                    message: "提交成功",
+                    type: "success"
+                  });
+                } else {
+                  this.$message({
+                    message: `err: ${res.msg}`,
+                    type: "error"
+                  });
+                }
+              });
+          })
+          .catch(() => {
+            this.$message({
+              type: "info",
+              message: "已取消"
+            });
+          });
+      } else {
+        console.log("error submit!!");
+        return false;
+      }
+    });
+  }
+  resetForm(formName: string) {
+    (this.$refs[formName] as LoginComponent).resetFields();
+  }
+}
 </script>
